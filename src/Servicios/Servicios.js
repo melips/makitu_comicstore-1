0f92337e841889711449ff35ce8a4a7950b2f6b1
melips <melipusu15098@gmail.com>
2023-04-25 02:28:44 -0500

    Se termina Home

--- src/Servicios/Servicios.js
+++ src/Servicios/Servicios.js
@@ -18,10 +18,21 @@ export function Servicios() {
 
           <div class="content">
 
+          <br></br>
+          <br></br>
+
           <h2>Comics</h2>
 
-          <h5 class="text">Nuestra tienda de cómics es el lugar ideal para los amantes de los cómics, ya que ofrecemos una amplia variedad de títulos que van desde los clásicos hasta las últimas novedades. Tenemos cómics de superhéroes de Marvel y DC, historias de ciencia ficción y fantasía, novelas gráficas y mucho más.  Si eres un coleccionista, también podrás encontrar ediciones especiales y rarezas difíciles de encontrar. </h5>
+          <h5 class="text">
+          
+          En nuestra tienda de cómics, contamos con una amplia selección de cómics de diferentes géneros y estilos para aquellos que disfrutan de la lectura como pasatiempo y como una forma de entretenimiento de calidad. Desde superhéroes hasta historias de ciencia ficción, nuestros cómics están cuidadosamente seleccionados para asegurarnos de que tengamos algo para todos los gustos. 
 
+           </h5>
+
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
             
           </div>
 
@@ -34,11 +45,24 @@ export function Servicios() {
 
           <div class="content">
  
+  
+      <br></br>
+      <br></br>
+
           <h2>Anime</h2>
 
           <h5 class="text">
-          Si eres un fanático del manga, no puedes perderte la increíble selección de títulos que encontrarás en nuestra tienda de cómics. Tenemos los últimos lanzamientos de las series más populares como Naruto, One Piece y Attack on Titan, así como clásicos como Dragon Ball y Sailor Moon. Además, también ofrecemos una gran variedad de géneros, desde romance y drama hasta acción y aventura. 
+
+            
+          Aqui puedes encontrar una sección dedicada al anime, que es perfecta para aquellos que disfrutan de la lectura como pasatiempo y como una forma de entretenimiento de calidad. En esta sección, encontrarás una amplia variedad de títulos de anime en forma de mangas y novelas ligeras, que están disponibles tanto en japonés como en inglés.
             </h5>
+
+      
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
             
           </div>
 
@@ -50,12 +74,22 @@ export function Servicios() {
           <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/Articulos.png?alt=media&token=4a4ae11f-0eb2-4252-b227-f840f4525cfe" alt="foto" class="img-flui"></img>
 
           <div class="content">
+            
+          <br></br>
+          <br></br>
 
           <h2>Articulos Coleccionables</h2>
 
           <h5 class="text">
-          Ofrecemos además una variedad de accesorios coleccionables para los fanáticos de los cómics y la cultura pop en general. Desde figuras de acción y estatuas hasta pósters y tarjetas coleccionables, contamos con una amplia gama de artículos ; te ofrecemos merchandising de tus personajes favoritos, y si eres un coleccionista ávido, tenemos ediciones limitadas y artículos exclusivos, difíciles de encontrar. 
+
+          Encontrarás además una sección dedicada a artículos coleccionables, perfecta para aquellos que quieren llevar su amor por los cómics a otro nivel. Nuestra selección de artículos coleccionables incluye figuras de acción, estatuas, juegos de mesa y cartas, entre otros. También contamos con una amplia variedad de objetos de colección de edición limitada, lo que significa que podrás encontrar piezas únicas y exclusivas que no encontrarás en ningún otro lugar.
+
             </h5>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
             
           </div>
 
@@ -72,34 +106,40 @@ export function Servicios() {
 
         <div class="col-12 col-md-3 fondo tituloS focus">
 
-          <h2>Comics</h2>
+          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-32b08.appspot.com/o/unnamed-removebg-preview.png?alt=media&token=f34e24f3-bc55-469b-9eb4-f215d89f9e60" alt="foto" class="img-flui icono2"></img>
 
-          <h5 class="text">Nuestra tienda de cómics es el lugar ideal para los amantes de los cómics, ya que ofrecemos una amplia variedad de títulos que van desde los clásicos hasta las últimas novedades. </h5>
+           <br></br>          
+
+          <h5 class="text"> También te ofrecemos una selección de cómics independientes, para aquellos que buscan algo un poco más alternativo. En resumen, si estás buscando pasar un buen rato leyendo cómics, nuestra tienda es el lugar perfecto para encontrar lo que estás buscando. </h5>
 
           
         </div>
 
         <div class="col-12 col-md-3 fondo tituloS focus">
 
-          <h2>Manga</h2>
+           <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-32b08.appspot.com/o/unnamed-removebg-preview%20(1).png?alt=media&token=e5a3354d-2052-4b3c-8909-9a528eea8b9b" alt="foto" class="img-flui icono2"></img>
 
-          <h5 class="text">
-          Si eres un fanático del manga, no puedes perderte la increíble selección de títulos que encontrarás en nuestra tienda de cómics.  
+            <br></br>  
+
+            <h5 class="text">
+            Desde clásicos del anime hasta los títulos más recientes, nuestra selección de anime está diseñada para satisfacer los gustos de cualquier aficionado a este género. Así que, si estás buscando pasar un buen rato leyendo, asegúrate de revisar nuestra sección de anime en la tienda.  
             </h5>
 
-          
-            
-          
+             
         </div>
 
         <div class="col-12 col-md-3 fondo tituloS focus">
 
 
+          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-32b08.appspot.com/o/unnamed-removebg-preview%20(2).png?alt=media&token=4b1a4184-cbbc-4fae-a40f-f0840bab9c9b" alt="foto" class="img-flui icono3"></img>
 
-          <h2>Articulos Coleccionables</h2>
+            <br></br>  
+          
 
           <h5 class="text">
-          Ofrecemos además una variedad de accesorios coleccionables para los fanáticos de los cómics y la cultura pop en general. 
+          
+          Si eres un verdadero coleccionista de cómics, nuestra sección de artículos coleccionables es el lugar perfecto para encontrar esa pieza especial que estás buscando para agregar a tu colección.
+
             </h5>
             
 
@@ -115,20 +155,20 @@ export function Servicios() {
 
         <div class="col-12 col-md-3 fondo tituloS container2">
 
-          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/ComicParteBaja.png?alt=media&token=5340613c-9d21-4eba-9ba0-6a803fc31aff" alt="foto" class="img-flui"></img>
+          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/ComicParteBaja.png?alt=media&token=5340613c-9d21-4eba-9ba0-6a803fc31aff" alt="foto" class="img-fluid"></img>
           
         </div>
 
         <div class="col-12 col-md-3 fondo tituloS container2">
 
-          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/AnimeParteBaja.png?alt=media&token=14d5c3c3-ee32-4e0a-8f69-413bd9a45acf" alt="foto" class="img-flui"></img>
+          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/AnimeParteBaja.png?alt=media&token=14d5c3c3-ee32-4e0a-8f69-413bd9a45acf" alt="foto" class="img-fluid"></img>
 
           
         </div>
 
         <div class="col-12 col-md-3 fondo tituloS container2">
 
-          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/ArticuloParteBaja.png?alt=media&token=80c0b28b-c5f9-45fb-bf7a-71ca667cfeec" alt="foto" class="img-flui"></img>
+          <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/ArticuloParteBaja.png?alt=media&token=80c0b28b-c5f9-45fb-bf7a-71ca667cfeec" alt="foto" class="img-fluid"></img>
 
           
         </div>
